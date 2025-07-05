42b0631fdb865111420a86c55236a98afe41a5f3
AgilHummatli <145222360+AgilHummatli@users.noreply.github.com>
2025-07-05 15:11:59 +0200

    Create factorial_while_loop.cpp

---
+++ factorial_while_loop.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem:
+ * Create a program to find the factorial of a positive integer using a while loop.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int total = 1;
+    int i = 1;
